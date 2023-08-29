55ab98798b04b93cef7142b8c16af6f070eed470
theuslinor <matheusdossantos3157@gmail.com>
2023-08-29 19:56:00 -0300

    Primeiro commit do curso

---
+++ Parte 1/PrimeiroEX/src/Main.java
@@ -0,0 +1,10 @@
+class Planet {
+    void revolve() {
+        System.out.println("Rev olve");
+    }
+    public static void main(String[] args){
+        Planet earth = new Planet();
+        earth.revolve();
+    }
+}
+
