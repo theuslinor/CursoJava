b134904c9029db822556179ba02b5879af6cec66
theuslinor <matheusdossantos3157@gmail.com>
2023-09-21 17:27:51 -0300

    Curso de JDBC

---
+++ Banco_JDBC/src/main/java/org/example/jdbc/CriarClasse.java
@@ -0,0 +1,24 @@
+package org.example.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CriarClasse {
+    public static void main(String[] args) throws SQLException {
+
+        final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
+        final String usuario = "root";
+        final String senha = "root";
+
+        Connection conexao = DriverManager.getConnection(url, usuario, senha);
+
+        Statement stmt = conexao.createStatement();
+        stmt.execute("CREATE DATABASE IF NOT EXISTS curso_java");
+
+        System.out.println("Banco criado com sucesso!");
+
+        conexao.close();
+    }
+}
