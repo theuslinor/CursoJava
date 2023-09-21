6e286b5feeca936634e65acfed98d0a9a7c6dec3
theuslinor <matheusdossantos3157@gmail.com>
2023-09-21 17:41:30 -0300

    fabrica de conexoes

---
+++ Banco_JDBC/src/main/java/org/example/jdbc/FabricaConexao.java
@@ -0,0 +1,20 @@
+package org.example.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+    public static Connection getConexao() {
+        try {
+            final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
+            final String usuario = "root";
+            final String senha = "root";
+
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
